a1e2a59cd5811a54749a02858fef4c418cfb284f
Lance Santos <lan30san@gmail.com>
2026-08-09 23:08:32 +1000

    Added home page and other images

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import { figtree } from './ui/fonts';
 import Image from 'next/image';
+import homeCSS from './ui/home.module.css';
 
 export default function Home() {
-  return <div>
-    <div className='flex items-center justify-center'>
-      <Image
-        src="/temp-card.png"
-        width={500}
-        height={700}
-        className="hidden md:block"
-        alt="Temporary card image"
-      />
+  return <div className='main main-tm'>
+    <h1 className={homeCSS.title}>Welcome to my page!!</h1>
+    <Image
+      src="/images/personal/dbz-ss.png"
+      width={680}
+      height={1360}
+      alt="Temporary card image"
+      className={homeCSS.image}
+    />
     </div>
-  </div>
 }
