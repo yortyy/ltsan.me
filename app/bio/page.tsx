11f3a7c72acd8119a2c7a292bbeb306f2d6a56ad
Lance Santos <lan30san@gmail.com>
2026-08-09 22:40:38 +1000

    Added new pages, home link

---
+++ app/bio/page.tsx
@@ -0,0 +1,16 @@
+import { figtree } from './ui/fonts';
+import Image from 'next/image';
+
+export default function Page() {
+  return <div>
+    <div className='flex items-center justify-center'>
+      <Image
+        src="/temp-card.png"
+        width={500}
+        height={700}
+        className="hidden md:block"
+        alt="Temporary card image"
+      />
+    </div>
+  </div>
+}
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -10,16 +10,18 @@ export default function NavBar() {
   const pathname = usePathname();
 
   return <div className={`${navStyles.navbar} ${figtree.className}`}>
-    <h1>ltsan.me</h1>
+    <Link href="/" className={clsx(navStyles.title, { [navStyles.active]: pathname === '/' })}>
+      ltsan.me
+    </Link>
     <div>
-      <Link href="/projects" className={clsx(navStyles.link, { [navStyles.active]: pathname === '/' })}>
+      <Link href="/projects" className={clsx(navStyles.link, { [navStyles.active]: pathname === '/projects' })}>
         Projects
       </Link>
-      <Link href="/bio" className={clsx(navStyles.link, { [navStyles.active]: pathname === '/about' })}>
+      <Link href="/bio" className={clsx(navStyles.link, { [navStyles.active]: pathname === '/bio' })}>
         Bio
       </Link>
     </div>
-    <Link href="/contact" className={clsx(navStyles.button, { [navStyles.active]: pathname === '/user' })}>
+    <Link href="/contact" className={clsx(navStyles.button, { [navStyles.active]: pathname === '/contact' })}>
       Contact
     </Link>
   </div>
